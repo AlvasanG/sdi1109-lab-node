fbb14fb59de2985ca43b77eb49d037b74b26409d
AlvasanG <varosanchezgar5@gmail.com>
2019-03-18 14:28:11 +0100

    SDI-NodeJS- 11,12,13,14 Vistas y motores de plantillas

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ app.use(express.static('public'));
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+var swig = require('swig');
 
 // Variables
 app.set('port', 8081);
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
 
 // lanzar el servidor
 app.listen(app.get('port'), function() {
