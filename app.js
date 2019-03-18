24f53e0b12b9be5141d3cf148b28a638709ea58b
AlvasanG <varosanchezgar5@gmail.com>
2019-03-18 13:36:19 +0100

    SDI-NodeJS-.3,4,5 Express, depuración y varibles de entorno

---
+++ app.js
@@ -0,0 +1,20 @@
+// Módulos
+var express = require('express');
+var app = express();
+
+// Variables
+app.set('port', 8081);
+
+app.get('/usuarios', function(req, res) {
+    console.log("Depurar aqui");
+    res.send('ver usuarios');
+});
+
+app.get('/canciones', function(req, res) {
+    res.send('ver canciones');
+});
+
+// lanzar el servidor
+app.listen(app.get('port'), function() {
+    console.log("Servidor activo");
+});
