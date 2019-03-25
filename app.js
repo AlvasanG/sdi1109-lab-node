fe94e980c3fccc86fed0700f8fd48fef245a2e01
Alvaro Sanchez Garcia <varosanchezgar5@gmail.com>
2019-03-25 10:44:52 +0100

    SDINodeJS-8.9 y 8.10 - Colecciones relacionadas y routers

--- app.js
+++ app.js
@@ -3,6 +3,42 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+var expressSession = require('express-session');
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.usuario ) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : "+req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar",routerUsuarioSession);
+app.use("/publicaciones",routerUsuarioSession);
+
+//routerAudios
+var routerAudios = express.Router();
+routerAudios.use(function(req, res, next) {
+    console.log("routerAudios");
+    var path = require('path');
+    var idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {id : mongo.ObjectID(idCancion) }, function (canciones) {
+            if(req.session.usuario && canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerAudios
+app.use("/audios/",routerAudios);
+
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -8,14 +8,14 @@ module.exports = function(app, swig, gestorBD) {
         res.send(respuesta);
     });
 
-    gestorBD.insertarUsuario(usuario, function(id) {
-        if (id == null){
-            res.send("Error al insertar ");
-        } else {
-            res.send('Usuario Insertado ' + id);
+    app.post('/usuario', function(req, res) {
+        var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+        var usuario = {
+            email : req.body.email,
+            password : seguro
         }
-    });
-
+    })
     app.get("/identificarse", function(req, res) {
         var respuesta = swig.renderFile('views/bidentificacion.html', {});
         res.send(respuesta);
